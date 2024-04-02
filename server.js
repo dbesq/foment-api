88d7a48b9d4ff76ac11cb3fed2b5fc0f5eb53df7
dbEsq <d_ber1@live.com>
2024-04-01 19:06:40 -0500

    move server.js

---
+++ server.js
@@ -0,0 +1,62 @@
+// Database
+import express from 'express' 
+import mongoose from 'mongoose'
+
+// External imports
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import serverless from 'serverless-http'
+
+// Import routes
+import authRoute from './functions/routes/auth.route.js'
+import conversationRoute from './functions/routes/conversation.route.js'
+import messageRoute from './functions/routes/message.route.js'
+import orderRoute from './functions/routes/order.route.js'
+import projectRoute from './functions/routes/project.route.js'
+import reviewRoute from './functions/routes/review.route.js'
+import userRoute from './functions/routes/user.route.js'
+
+const app = express()
+
+dotenv.config()
+
+// Connect to DB
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO) 
+        console.log('Connected to MongoDB')
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// Middleware
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
+app.use(express.json())
+app.use(cookieParser())
+
+// Routes
+app.use('/api/auth', authRoute)
+app.use('/api/conversations', conversationRoute)
+app.use('/api/messages', messageRoute)
+app.use('/api/orders', orderRoute)
+app.use('/api/projects', projectRoute)
+app.use('/api/reviews', reviewRoute)
+app.use('/api/users', userRoute)
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    const errorStatus = err.status || 500
+    const errorMessage = err.message || 'Something went wrong.'
+
+    res.status(errorStatus).send(errorMessage)
+})
+
+// app.listen(8800, () => {
+//     connect()
+//     console.log('Backend server is running!!')
+// })
+
+export const handler = serverless(app)
+
